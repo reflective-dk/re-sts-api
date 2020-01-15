7e046b77ce15ca215b43dbef33109401fdf67a25
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2020-01-15 19:32:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# re-sts-api #
+
+API for using KOMBIT Støttesystemerne (STS) with Reflective.
+
+## Configuration
+
+The endpoints of services have default values, but you can override them in the
+constructor.
+
+```js
+const StsApi = require('re-sts-api');
+var stsApi = new StsApi({
+  stsHost: 'http://some-sts-host'  // this option must be specified
+});
+```
+
+## Usage
+
+Arguments and objects passed in and out of this API are instances of the classes
+found in `re-models/models/sts-*/classes`. They follow the usual Reflective
+object and snapshot structure.
+
+```js
+stsApi.readOrgUnits(orgUnitIds)
+  => returns a promise that resolves to a map of objects
+  {
+      'some-uuid': {
+          id: 'some-uuid',
+          snapshot: {
+              name: 'Name of Org Unit',
+              Overordnet: { id: 'overordnet-uuid' }
+          }
+      }
+  }
+```
---
+++ test/laes-test.js
@@ -0,0 +1,131 @@
+"use strict";
+
+global.global_logger = {
+    debug: arg => arg ? console.log(arg) : true
+};
+var Promise = require('bluebird');
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+
+var StsApi = require('../lib/sts-api');
+var stsApi = new StsApi({
+    stsHost: 'http://mock-sts-host',
+    request: mockRequestLib()
+});
+
+describe('Read operations', function() {
+    describe('readAddresses', function() {
+        it('should return addresses', function(done) {
+            expect(stsApi.readAddresses([
+                'address0', 'address1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readAuthorities', function() {
+        it('should return authorities', function(done) {
+            expect(stsApi.readAuthorities([
+                'authority0', 'authority1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readBusinesses', function() {
+        it('should return businesses', function(done) {
+            expect(stsApi.readBusinesses([
+                'business0', 'business1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readInterestGroups', function() {
+        it('should return interest groups', function(done) {
+            expect(stsApi.readInterestGroups([
+                'intgroup0', 'intgroup1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readITSystems', function() {
+        it('should return IT systems', function(done) {
+            expect(stsApi.readITSystems([
+                'itsystem0', 'itsystem1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readOrganizations', function() {
+        it('should return organizations', function(done) {
+            expect(stsApi.readOrganizations([
+                'organization0', 'organization1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readOrgFunctions', function() {
+        it('should return org functions', function(done) {
+            expect(stsApi.readOrgFunctions([
+                'orgfun0', 'orgfun1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readOrgUnits', function() {
+        it('should return org units', function(done) {
+            expect(stsApi.readOrgUnits([
+                'orgunit0', 'orgunit1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readPeople', function() {
+        it('should return people', function(done) {
+            expect(stsApi.readPeople([
+                'person0', 'person1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+    describe('readUsers', function() {
+        it('should return users', function(done) {
+            expect(stsApi.readUsers([
+                'user0', 'user1', 'unknown-object'
+            ]))
+            .to.eventually.deep.equal({
+            })
+            .notify(done);
+        });
+    });
+
+});
+
+function mockRequestLib() {
+}
